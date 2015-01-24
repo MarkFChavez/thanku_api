bb913c51aba545250a4fa1fe3a38c3042a8fd5a9
Mark Chavez <markchav3z@gmail.com>
2015-01-24 10:02:20 +0800

    Setup flask decorator for testing

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
+from flask import Flask, Blueprint
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
 
 api = Blueprint("api", __name__)
+
+def create_app(config_name):
+  from .api_1_0 import api as api_1_0_blueprint
+  app.register_blueprint(api_1_0_blueprint, url_prefix="/api/v1.0")
+
+@app.route("/")
+def index():
+  return "Hello world!"
+
+if __name__ == "__main__":
+  manager.run()
