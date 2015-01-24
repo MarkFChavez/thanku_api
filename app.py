5c08856fe7ca293f68a1f49bd62e0d120596a6aa
Mark Chavez <markchav3z@gmail.com>
2015-01-24 19:57:50 +0800

    Setup initial authentication scheme

--- app.py
+++ app.py
@@ -1,22 +1,24 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
 from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
+from passlib.apps import custom_app_context as pwd_context
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
-app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
-db = SQLAlchemy(app)
-manager = Manager(app)
+api = Flask(__name__)
+api.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+api.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+
+db = SQLAlchemy(api)
+manager = Manager(api)
 
 class User(db.Model):
   __tablename__ = "users"
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(255), unique=True)
-  email = db.Column(db.String(255), unique=True)
-  password = db.Column(db.String(255))
+  username = db.Column(db.String(64), unique=True)
+  name = db.Column(db.String(128))
+  password_hash = db.Column(db.String(128))
 
   def __repr__(self):
     return "<User %r>" % self.name
@@ -29,13 +31,6 @@ class User(db.Model):
     }
 
     return json_user
-
-@app.route("/api/v1.0/users")
-def index():
-  return jsonify({ "users": [user.to_json() for user in users()] })
-
-def users():
-  return User.query.all()
 
 if __name__ == "__main__":
   manager.run()
