d5cc3c7a29fd110fdbbc791bfc9beb85caa51c38
Mark Chavez <markchav3z@gmail.com>
2015-01-24 19:01:56 +0800

    Add user model

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
+import os
 from flask import Flask
 from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "data.sqlite")
+app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = True
+db = SQLAlchemy(app)
 manager = Manager(app)
+
+class User(db.Model):
+  __tablename__ = "users"
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(255), unique=True)
+  email = db.Column(db.String(255), unique=True)
+  password = db.Column(db.String(255))
+
+  def __repr__(self):
+    return "<User %r>" % self.name
 
 if __name__ == "__main__":
   manager.run()
